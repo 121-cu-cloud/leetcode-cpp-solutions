be7b2412a988e8915d0d6978054b29297ad21f28
Nthabiseng Juliet Lefielo <23BDA70121@cuchd.in>
2026-01-21 14:24:56 +0530

    Create Median of two sorted arrays.cpp

---
+++ Median of two sorted arrays.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int m = nums1.size();
+    int n = nums2.size();
+    vector<int> merged(m + n);
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < m && j < n) {
+        if (nums1[i] <= nums2[j]) {
+            merged[k++] = nums1[i++];
+        } else {
+            merged[k++] = nums2[j++];
+        }
+    }
+
+    while (i < m) merged[k++] = nums1[i++];
+    while (j < n) merged[k++] = nums2[j++];
+
+    int total = m + n;
+    if (total % 2 == 0) {
+        return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
+    } else {
+        return merged[total / 2];
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    double median = findMedianSortedArrays(nums1, nums2);
+    cout << "Median: " << median << endl;
+
+    return 0;
+}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int m = nums1.size();
+    int n = nums2.size();
+    vector<int> merged(m + n);
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < m && j < n) {
+        if (nums1[i] <= nums2[j]) {
+            merged[k++] = nums1[i++];
+        } else {
+            merged[k++] = nums2[j++];
+        }
+    }
+
+    while (i < m) merged[k++] = nums1[i++];
+    while (j < n) merged[k++] = nums2[j++];
+
+    int total = m + n;
+    if (total % 2 == 0) {
+        return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
+    } else {
+        return merged[total / 2];
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    double median = findMedianSortedArrays(nums1, nums2);
+    cout << "Median: " << median << endl;
+
+    return 0;
+}
